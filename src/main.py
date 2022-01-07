432bda7ed33c5a9daa612cd1491d3b301f0b3a2b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2022-01-07 08:28:51 +0000

    style: automatic code formatting with linter

--- src/main.py
+++ src/main.py
@@ -3,21 +3,24 @@
 
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')   # Add a touch of color
+sg.theme("DarkAmber")  # Add a touch of color
 # All the stuff inside your window.
-layout = [[sg.Text('Some text on Row 1')],
-          [sg.Text('Enter something on Row 2'), sg.InputText()],
-          [sg.Button('Ok'), sg.Button('Cancel')]]
+layout = [
+    [sg.Text("Some text on Row 1")],
+    [sg.Text("Enter something on Row 2"), sg.InputText()],
+    [sg.Button("Ok"), sg.Button("Cancel")],
+]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window("Window Title", layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event in [
-            sg.WIN_CLOSED,
-            'Cancel']:  # if user closes window or clicks cancel
+        sg.WIN_CLOSED,
+        "Cancel",
+    ]:  # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    print("You entered ", values[0])
 
 window.close()
